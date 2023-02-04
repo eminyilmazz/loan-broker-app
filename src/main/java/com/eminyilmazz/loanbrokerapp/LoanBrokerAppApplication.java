3138aa4eccfee7a7f2b3e972982084f220892a50
Emin <eminyilmzz@gmail.com>
2023-02-04 16:40:40 +0300

    Initial commit

---
+++ src/main/java/com/eminyilmazz/loanbrokerapp/LoanBrokerAppApplication.java
@@ -0,0 +1,13 @@
+package com.eminyilmazz.loanbrokerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanBrokerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanBrokerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eminyilmazz/loanbrokerapp/LoanBrokerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminyilmazz.loanbrokerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanBrokerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
