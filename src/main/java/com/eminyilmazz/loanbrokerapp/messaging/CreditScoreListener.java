7b1963ac575642c003a7be2844f212e532017c11
Emin <eminyilmzz@gmail.com>
2023-02-05 22:47:20 +0300

    Loggers

--- src/main/java/com/eminyilmazz/loanbrokerapp/messaging/CreditScoreListener.java
+++ src/main/java/com/eminyilmazz/loanbrokerapp/messaging/CreditScoreListener.java
@@ -2,14 +2,17 @@ package com.eminyilmazz.loanbrokerapp.messaging;
 
 import com.eminyilmazz.loanbrokerapp.config.RabbitConfig;
 import com.eminyilmazz.loanbrokerapp.utility.CreditScoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreditScoreListener {
-
+    private static final Logger logger = LoggerFactory.getLogger(CreditScoreListener.class);
     @RabbitListener(queues = RabbitConfig.CREDIT_QUEUE)
     private Integer creditScoreListener(Long tckn){
+        logger.trace("Credit score request received for: {}", tckn);
         return CreditScoreService.calculateCreditScore(tckn);
     }
 }
--- src/main/java/com/eminyilmazz/loanbrokerapp/messaging/CreditScoreProducer.java
+++ src/main/java/com/eminyilmazz/loanbrokerapp/messaging/CreditScoreProducer.java
@@ -1,17 +1,20 @@
 package com.eminyilmazz.loanbrokerapp.messaging;
 
 import com.eminyilmazz.loanbrokerapp.config.RabbitConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreditScoreProducer {
-
+    private static final Logger logger = LoggerFactory.getLogger(CreditScoreProducer.class);
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public Integer send(Long value) {
+        logger.trace("Sending credit score request for: {}", value);
         return (Integer) rabbitTemplate.convertSendAndReceive(RabbitConfig.EXCHANGE, RabbitConfig.CREDIT_QUEUE, value);
     }
 }
