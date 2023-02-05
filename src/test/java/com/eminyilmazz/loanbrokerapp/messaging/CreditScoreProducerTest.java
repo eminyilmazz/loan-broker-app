23b4dcf8592c77535e559bf070d1ed6aefe0b687
Emin <eminyilmzz@gmail.com>
2023-02-05 22:37:31 +0300

    CreditScoreProducer test

---
+++ src/test/java/com/eminyilmazz/loanbrokerapp/messaging/CreditScoreProducerTest.java
@@ -0,0 +1,21 @@
+package com.eminyilmazz.loanbrokerapp.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CreditScoreProducerTest {
+    @Autowired
+    private CreditScoreProducer creditScoreProducer;
+
+    @Test
+    public void testSendMessage() {
+        Long tckn = 12345678910L;
+        Integer returnedValue = creditScoreProducer.send(tckn);
+        assertEquals(1000, returnedValue);
+    }
+}
