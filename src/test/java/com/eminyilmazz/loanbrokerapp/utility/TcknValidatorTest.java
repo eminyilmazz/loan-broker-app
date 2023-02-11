491e11a28a89ddc8a20bb0f35d5eb7d12e5d8c1c
Emin <eminyilmzz@gmail.com>
2023-02-11 18:46:13 +0300

    test: unit - TcknValidator validate

---
+++ src/test/java/com/eminyilmazz/loanbrokerapp/utility/TcknValidatorTest.java
@@ -0,0 +1,25 @@
+package com.eminyilmazz.loanbrokerapp.utility;
+
+import com.eminyilmazz.loanbrokerapp.exception.IllegalTcknException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TcknValidatorTest {
+
+    @Test
+    void validate_withValidTckn_shouldPass() {
+        long tckn = 12345678910L;
+
+        TcknValidator.validate(tckn);
+    }
+
+    @Test
+    void validate_withInvalidTckn_shouldThrowException() {
+        long tckn = 1234567890L;
+
+        Exception exception = assertThrows(IllegalTcknException.class, () -> TcknValidator.validate(tckn));
+        assertEquals(IllegalTcknException.class, exception.getClass());
+        assertEquals("TCKN needs to be 11 digits and can only contain only numbers.", exception.getMessage());
+    }
+}
