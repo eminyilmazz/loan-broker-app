9c99b9af83cf62be68fda7f89934fb46217d2919
Emin <eminyilmzz@gmail.com>
2023-02-13 01:52:40 +0300

    feat: comment CreditScoreService

--- src/main/java/com/eminyilmazz/loanbrokerapp/utility/CreditScoreService.java
+++ src/main/java/com/eminyilmazz/loanbrokerapp/utility/CreditScoreService.java
@@ -5,6 +5,14 @@ public class CreditScoreService {
     private CreditScoreService() {
     }
 
+    /**
+     * This static method only simulates a credit score service that is assumed to already exist.
+     * Returns the last three digits of TCKN as an integer.
+     * If the last three digits of TCKN is greater than 900, returns 1000.
+     *
+     * @param tckn 11-digit ID of Customer.class.
+     * @return An Integer valuing between 0 and 1000.
+     */
     public static Integer calculateCreditScore(Long tckn) {
         int creditScore = Math.toIntExact(tckn % 1000);
         return creditScore > 900 ? 1000 : creditScore;
