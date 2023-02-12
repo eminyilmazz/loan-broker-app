71a9f5e3a3f2e5c7d0697620006e6edc1072fd7e
Emin <eminyilmzz@gmail.com>
2023-02-12 19:43:22 +0300

    fix: loanApplication birthDate validation

--- src/main/java/com/eminyilmazz/loanbrokerapp/model/dto/LoanApplicationDto.java
+++ src/main/java/com/eminyilmazz/loanbrokerapp/model/dto/LoanApplicationDto.java
@@ -2,6 +2,7 @@ package com.eminyilmazz.loanbrokerapp.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -14,6 +15,7 @@ public class LoanApplicationDto {
     @NotNull(message = "TCKN cannot be empty.")
     private Long tckn;
     @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Birth date needs be in this format: \"yyyy-MM-dd\"")
+    @NotBlank
     @JsonProperty(value = "birth_date")
     private String birthDate;
     private double assurance;
